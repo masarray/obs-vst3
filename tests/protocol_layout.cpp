46e13be0942fbedcdc6813f5798d026adbea4f33
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-23 18:42:44 +0700

    test: validate protocol v2 control capacity

--- tests/protocol_layout.cpp
+++ tests/protocol_layout.cpp
@@ -6,14 +6,19 @@
 int main()
 {
     using namespace safevst3;
+    static_assert(kProtocolVersion == 2);
     static_assert(kSlotCount >= 4);
     static_assert(kMaxFrames >= 1024);
+    static_assert(kMaxParameters >= 128);
+    static_assert(alignof(ParameterDescriptor) >= 64);
     static_assert(alignof(AudioSlot) >= 64);
     static_assert(alignof(SharedAudioRegion) >= 64);
     assert(sizeof(SharedAudioRegion) < 256 * 1024);
     SharedAudioRegion region{};
     assert(region.magic == kProtocolMagic);
     assert(region.version == kProtocolVersion);
+    assert(region.parameter_count == 0);
+    assert(region.parameter_total_count == 0);
     std::cout << "protocol v" << region.version << ", shared region " << sizeof(region) << " bytes\n";
     return 0;
 }
