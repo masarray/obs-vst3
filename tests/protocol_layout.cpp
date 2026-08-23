c2b9529ff41465ece7b4c8bf1243ff6a96f58ff9
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-23 20:10:00 +0700

    test: cover recovered status ABI

--- tests/protocol_layout.cpp
+++ tests/protocol_layout.cpp
@@ -6,10 +6,11 @@
 int main()
 {
     using namespace safevst3;
-    static_assert(kProtocolVersion == 2);
+    static_assert(kProtocolVersion == 3);
     static_assert(kSlotCount >= 4);
     static_assert(kMaxFrames >= 1024);
     static_assert(kMaxParameters >= 128);
+    static_assert(kPluginNameBytes >= 64);
     static_assert(alignof(ParameterDescriptor) >= 64);
     static_assert(alignof(AudioSlot) >= 64);
     static_assert(alignof(SharedAudioRegion) >= 64);
@@ -19,6 +20,8 @@ int main()
     assert(region.version == kProtocolVersion);
     assert(region.parameter_count == 0);
     assert(region.parameter_total_count == 0);
+    assert(region.latency_samples == 0);
+    assert(region.plugin_name[0] == '\0');
     assert(region.editor_command == static_cast<long>(EditorCommand::None));
     assert(region.editor_status == static_cast<long>(EditorStatus::Unknown));
     assert(region.editor_request_generation == 0);
